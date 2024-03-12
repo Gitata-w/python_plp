c2e7fc27e66dbb7e280c532368afed543f55ad31
Gitata-w <wgitata@gmail.com>
2024-03-12 15:50:19 +0300

    Input Task

---
+++ user_input.py
@@ -0,0 +1,20 @@
+import sys
+
+#Ask user for their name
+
+print("Enter your name :")
+name = input()
+
+#Ask user for their age
+
+print("Enter you age :")
+age = input()
+
+#Asking user for their location
+
+print("Enter your location:")
+location = input()
+
+#Prints out personalized message
+
+print("Hello," +name, "you are " +age, "years old and live in " +location)
